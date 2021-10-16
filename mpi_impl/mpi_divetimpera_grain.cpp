e16e1a0a25f1a2aa42df40da2f7f59221c9d6076
LanderOtto <alberto.mulone@edu.unito.it>
2021-10-16 09:44:41 +0200

    divided implementation of mpi divetimpera in two versions

--- mpi_impl/mpi_divetimpera_grain.cpp
+++ mpi_impl/mpi_divetimpera_grain.cpp
@@ -21,6 +21,8 @@ P0 P1 P2 P3 P4 P5 P6 P7
 using std::cout;
 using std::endl;
 
+// it must be a negative number.
+#define EXIT_VAL -8
 
 int main(int argc, char** argv) {
     int myid;
@@ -48,74 +50,84 @@ int main(int argc, char** argv) {
     printf("i am %d on %s\n", myid, processor_name);
     MPI_Barrier(MPI_COMM_WORLD);
 
-    int cursor [2]; // cursor[0]: span, cursor[1]: array_len
+    void **cursor = (void **) malloc(2 * sizeof(void *));
+    cursor[0] = &span;
+    cursor[1] = &array_len;
 
     if (myid == 0) {
       fflush(stdout);
       array = common_begin(argc, argv, &array_len, &grain, NULL);
-      cursor[1] = (int) array_len;
       ch.start_chrono();
 
-      tmp_array = (int *) malloc(cursor[1] * sizeof(int));
+      tmp_array = (int *) malloc(array_len * sizeof(int));
 
       // span between the myid and the id of the other processes based on the level of the tree you are working on
-      cursor[0] = num_procs / 2;
-      init_span = cursor[0];
+      span = num_procs / 2;
+      init_span = span;
     } else {
       // processes in the internal nodes of the tree: receive all data (span, array_len and array)
       MPI_Recv(&cursor, 2, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      if(span == EXIT_VAL){ // exit value
+        MPI_Finalize();
+        return 0;
+      }
       mitt = status.MPI_SOURCE % num_procs;
-      init_span = cursor[0];
-      array = (int *) malloc(cursor[1] * sizeof(int));
-      tmp_array = (int *) malloc(cursor[1] * sizeof(int));
-      MPI_Recv(array, cursor[1], MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      init_span = span;
+      array = (int *) malloc(array_len * sizeof(int));
+      tmp_array = (int *) malloc(array_len * sizeof(int));
+      MPI_Recv(array, array_len, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     }
 
-    dest = myid + cursor[0];
-    cursor[0] = cursor[0] / 2;
-    while(dest > myid){   // when span to previous cycle was zero
+    dest = myid + span;
+    span = span / 2;
+    while(array_len > grain && dest > myid){   // when span to previous cycle was zero
       // divide array
-      cursor[1] = cursor[1] / 2;
+      array_len = array_len / 2;
 
       // send second half of array
       MPI_Send(&cursor, 2, MPI_INT, dest, 0, MPI_COMM_WORLD);
-      MPI_Send(&array[cursor[1]], cursor[1], MPI_INT, dest, 0, MPI_COMM_WORLD);
+      MPI_Send(&array[array_len], array_len, MPI_INT, dest, 0, MPI_COMM_WORLD);
 
       // calculation of the id of the next process
-      dest = myid + cursor[0];
-      cursor[0] = cursor[0] / 2;
+      dest = myid + span;
+      span = span / 2;
     }
 
     // only if it sent to all process
-    if(cursor[0] == 0) {
-      cursor[0] = 1;
-      dest = myid + cursor[0];
+    if(span == 0) {
+      span = 1;
+      dest = myid + span;
     }
 
     // sorted first half of array
-    merge_sort(array, tmp_array, 0, cursor[1] - 1);
+    merge_sort(array, tmp_array, 0, array_len - 1);
 
-    while(cursor[0] <= init_span){
+    while(array_len > grain && span <= init_span){
       // get the second half of the array computed by the other process
-      MPI_Recv(&array[cursor[1]], cursor[1], MPI_INT, dest, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(&array[array_len], array_len, MPI_INT, dest, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
       // combines the two ordered subarrays
-      cursor[1] = 2 * cursor[1];
-      merge(array, tmp_array, 0, ((cursor[1] - 1) / 2), cursor[1] - 1);
+      array_len = 2 * array_len;
+      merge(array, tmp_array, 0, ((array_len - 1) / 2), array_len - 1);
 
       // calculation of the id of the previous process
-      cursor[0] = cursor[0] * 2;
-      dest = cursor[0] + myid;
+      span = span * 2;
+      dest = span + myid;
     }
 
     if(myid == 0) {
+      int exit = EXIT_VAL;
+      for(int i = 1; i < num_procs; i++){
+        MPI_Send(&exit, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+      }
+
       ch.end_chrono();
-      common_end(ch.get_diff(), array, cursor[1]);
+      common_end(ch.get_diff(), array, array_len);
 
     } else {
       // send subarray sorted
-      MPI_Send(array, cursor[1], MPI_INT, mitt, 0, MPI_COMM_WORLD);
+      MPI_Send(array, array_len, MPI_INT, mitt, 0, MPI_COMM_WORLD);
     }
 
     free(array);
